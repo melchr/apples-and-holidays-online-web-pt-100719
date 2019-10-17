06c11ddc322ec250bc7c79d77307c555d979e16d
Melissa Christman <melchrstmn@gmail.com>
2019-10-17 23:11:44 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -11,10 +11,9 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
 holiday_hash.each do |season, holiday|
   if season == "winter"
     holiday.each do |attribute, data|
-      << "supply"
+      holiday << "supply"
     end
   end
-end
 end
 
 
