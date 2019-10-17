3b7bf6c8517ed35b86ef4bf72686343b5556793b
Melissa Christman <melchrstmn@gmail.com>
2019-10-17 23:09:44 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -10,7 +10,8 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
 holiday_hash.each do |season, holiday|
   if season == "winter"
-    holiday.each do 
+    holiday.each do |attribute, data|
+      
 end
 
 
